4ec58ff1e986976df87b241c747db0b7519b98f6
Chris Coley <chris@codingallnight.com>
2015-12-30 22:18:50 -0800

    Adding automated testing

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+		qunit: {
+			all: ['tests/*.html']
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.registerTask('default', ['qunit']);
+}
